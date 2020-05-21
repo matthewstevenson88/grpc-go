7a8b340089d4c9e367811ad70585ed3ea71c3663
Ryan Kim <ryanfsdf@live.ca>
2020-05-21 21:47:20 +0000

    Address review comments

--- security/s2a/internal/crypter/aeadcrypter.go
+++ security/s2a/internal/crypter/aeadcrypter.go
@@ -3,12 +3,10 @@ package crypter
 // S2AAeadCrypter is the interface for an AEAD cipher used by the S2A record
 // protocol.
 type S2AAeadCrypter interface {
-	// Encrypt encrypts plaintext, computes the tag of plaintext, and appends
-	// the resulting ciphertext to dst, returning the updated slice. dst and
-	// plaintext may fully overlap or not at all.
+	// Encrypt encrypts the plaintext and computes the tag of dst and plaintext.
+	// dst and plaintext may fully overlap or not at all.
 	Encrypt(dst, plaintext []byte) ([]byte, error)
-	// Decrypt decrypts ciphertext, verifies the tag, and appends the resulting
-	// plaintext to dst, returning the updated slice. dst and ciphertext may
+	// Decrypt decrypts ciphertext and verifies the tag. dst and ciphertext may
 	// fully overlap or not at all.
 	Decrypt(dst, ciphertext []byte) ([]byte, error)
 	// TagSize returns the tag size in bytes.
