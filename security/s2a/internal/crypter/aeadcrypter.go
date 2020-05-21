2fcad3fe3f809b7597339e878c24852c2775a6f4
Ryan Kim <Ryanfsdf@users.noreply.github.com>
2020-05-21 21:52:01 +0000

    Add AEAD interface
    Added the AEAD interface. This largely resembles [ALTSRecordCrypto](https://github.com/grpc/grpc-go/blob/695da821e98cb7a10c41e3f5008aa994560e551a/credentials/alts/internal/conn/record.go#L33).
    
    The differences between this and `ALTSRecordCrypto` include:
    - the name of the interface
    - `EncryptionOverhead` -> `TagSize`
    - addition of `UpdateKey API

---
+++ security/s2a/internal/crypter/aeadcrypter.go
@@ -0,0 +1,16 @@
+package crypter
+
+// S2AAeadCrypter is the interface for an AEAD cipher used by the S2A record
+// protocol.
+type S2AAeadCrypter interface {
+	// Encrypt encrypts the plaintext and computes the tag of dst and plaintext.
+	// dst and plaintext may fully overlap or not at all.
+	Encrypt(dst, plaintext []byte) ([]byte, error)
+	// Decrypt decrypts ciphertext and verifies the tag. dst and ciphertext may
+	// fully overlap or not at all.
+	Decrypt(dst, ciphertext []byte) ([]byte, error)
+	// TagSize returns the tag size in bytes.
+	TagSize() int
+	// UpdateKey updates the key used for encryption and decryption.
+	UpdateKey(key []byte) error
+}
