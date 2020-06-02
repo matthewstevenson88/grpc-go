f5e26eec5f6e8c8b7e8088be7a86b3da544d12b4
Ryan Kim <ryanfsdf@live.ca>
2020-06-02 15:24:55 -0600

    Address review comments

--- security/s2a/internal/crypter/expander.go
+++ security/s2a/internal/crypter/expander.go
@@ -1,10 +1,12 @@
 package crypter
 
-import "hash"
+import (
+	"hash"
+)
 
-// HKDFExpander is the interface for the HKDF expansion function; see
+// hkdfExpander is the interface for the HKDF expansion function; see
 // https://tools.ietf.org/html/rfc5869 for details. It's use in TLS 1.3 is specified in
 // https://tools.ietf.org/html/rfc8446#section-7.2
-type HKDFExpander interface {
-	Expand(hash func() hash.Hash, secret, label []byte, length int) ([]byte, error)
+type hkdfExpander interface {
+	expand(hash func() hash.Hash, secret, label []byte, length int) ([]byte, error)
 }
