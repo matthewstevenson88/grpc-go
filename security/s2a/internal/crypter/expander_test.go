77945c12e9358b249259920fc2640fa5f7243dd3
Ryan Kim <ryanfsdf@live.ca>
2020-06-04 16:26:56 -0600

    Rename test vector struct fields

--- security/s2a/internal/crypter/expander_test.go
+++ security/s2a/internal/crypter/expander_test.go
@@ -10,38 +10,40 @@ import (
 func TestExpand(t *testing.T) {
 	// The following test vectors were taken from
 	// https://tools.ietf.org/html/rfc5869. Note that they have been
-	// slightly modified to fit our interface and implementation.
+	// slightly modified to fit our interface and implementation. Also note
+	// that `prk` and `okm` mentioned in the RFC have been renamed to `secret`
+	// and `out`.
 	for _, tc := range []struct {
-		desc           string
-		prk, info, okm []byte
-		length         int
+		desc              string
+		secret, info, out []byte
+		length            int
 	}{
 		{
-			desc: "sha256 basic",
-			prk:  testutil.Dehex("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5"),
-			info: testutil.Dehex("f0f1f2f3f4f5f6f7f8f9"),
-			okm:  testutil.Dehex("3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf"),
+			desc:   "sha256 basic",
+			secret: testutil.Dehex("077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5"),
+			info:   testutil.Dehex("f0f1f2f3f4f5f6f7f8f9"),
+			out:    testutil.Dehex("3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf"),
 		},
 		{
-			desc: "sha256 longer input/output",
-			prk:  testutil.Dehex("06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244"),
-			info: testutil.Dehex("b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"),
-			okm:  testutil.Dehex("b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c"),
+			desc:   "sha256 longer input/output",
+			secret: testutil.Dehex("06a6b88c5853361a06104c9ceb35b45cef760014904671014a193f40c15fc244"),
+			info:   testutil.Dehex("b0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"),
+			out:    testutil.Dehex("b11e398dc80327a1c8e7f78c596a49344f012eda2d4efad8a050cc4c19afa97c"),
 		},
 		{
-			desc: "sha256 zero length info",
-			prk:  testutil.Dehex("19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04"),
-			okm:  testutil.Dehex("8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d"),
+			desc:   "sha256 zero length info",
+			secret: testutil.Dehex("19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04"),
+			out:    testutil.Dehex("8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d"),
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
 			expander := newDefaultHKDFExpander()
-			got, err := expander.expand(sha256.New, tc.prk, tc.info)
+			got, err := expander.expand(sha256.New, tc.secret, tc.info)
 			if err != nil {
 				t.Errorf("expand failed with error: %v", err)
 			}
-			if !bytes.Equal(got, tc.okm) {
-				t.Errorf("expand(sha256.New, %v, %v) = %v, want %v.", tc.prk, tc.info, got, tc.okm)
+			if !bytes.Equal(got, tc.out) {
+				t.Errorf("expand(sha256.New, %v, %v) = %v, want %v.", tc.secret, tc.info, got, tc.out)
 			}
 		})
 	}
