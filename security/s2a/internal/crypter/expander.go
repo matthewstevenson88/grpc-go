f577c4fc7b8d66c302139c3cb6e61889beb4372e
Ryan Kim <ryanfsdf@live.ca>
2020-06-04 12:21:33 -0600

    Address review comments

--- security/s2a/internal/crypter/expander.go
+++ security/s2a/internal/crypter/expander.go
@@ -1,6 +1,8 @@
 package crypter
 
 import (
+	"fmt"
+	"golang.org/x/crypto/hkdf"
 	"hash"
 )
 
@@ -11,4 +13,25 @@ type hkdfExpander interface {
 	// expand takes a hashing function, a secret, a label, and an output length
 	// in number of bytes, and returns the resulting expanded key.
 	expand(hash func() hash.Hash, secret, label []byte, length int) ([]byte, error)
+}
+
+// defaultHKDFExpander is the default HKDF expander which uses Go's crypto/hkdf
+// for HKDF expansion.
+type defaultHKDFExpander struct{}
+
+// newDefaultHKDFExpander creates an instance of the default HKDF expander.
+func newDefaultHKDFExpander() hkdfExpander {
+	return &defaultHKDFExpander{}
+}
+
+func (*defaultHKDFExpander) expand(hash func() hash.Hash, secret, label []byte, length int) ([]byte, error) {
+	outBuf := make([]byte, length)
+	n, err := hkdf.Expand(hash, secret, label).Read(outBuf)
+	if err != nil {
+		return nil, fmt.Errorf("hkdf.expand.Read failed with error: %v", err)
+	}
+	if n != length {
+		return nil, fmt.Errorf("hkdf.expand.Read returned unexpected length, got: %d, expected %d", n, length)
+	}
+	return outBuf, nil
 }
