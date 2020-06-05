3a2e58e389fa47e20f9e8bed3cc25cab44ed47f8
Ryan Kim <ryanfsdf@live.ca>
2020-06-05 15:43:45 -0600

    Change inc and val to getAndIncrement

--- security/s2a/internal/crypter/counter.go
+++ security/s2a/internal/crypter/counter.go
@@ -18,30 +18,22 @@ func newCounter(value uint64) counter {
 	return counter{value: value}
 }
 
-// val returns the current value of the counter as a byte slice.
-func (c *counter) val() ([]byte, error) {
+// getAndIncrement returns the current value of the counter as a byte slice,
+// and increments the underlying value.
+func (c *counter) getAndIncrement() ([]byte, error) {
 	if c.hasOverflowed {
 		return nil, errors.New("invalid counter due to overflow")
 	}
 	buf := make([]byte, counterLen)
 	binary.LittleEndian.PutUint64(buf, c.value)
-	return buf, nil
-}
-
-// inc increments the counter and checks for overflow.
-func (c *counter) inc() {
-	// If the counter is already invalid due to overflow, there is no need to
-	// increase it. We check for the hasOverflowed flag in the call to val().
-	if c.hasOverflowed {
-		return
-	}
 	c.value++
 	if c.value == 0 {
 		c.hasOverflowed = true
 	}
+	return buf, nil
 }
 
-// reset sets the counter value to zero and resets the hasOverflowed flag.
+// reset sets the counter value to zero and sets hasOverflowed to false.
 func (c *counter) reset() {
 	c.value = 0
 	c.hasOverflowed = false
