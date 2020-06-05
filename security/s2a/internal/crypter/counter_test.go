4f8beefc65cd5dd33e75ec999ce7f076f1235286
Ryan Kim <ryanfsdf@live.ca>
2020-06-05 15:43:45 -0600

    Change inc and val to getAndIncrement

--- security/s2a/internal/crypter/counter_test.go
+++ security/s2a/internal/crypter/counter_test.go
@@ -12,12 +12,14 @@ func counterFromValue(value []byte) (c counter) {
 	return newCounter(binary.LittleEndian.Uint64(value))
 }
 
+type counterTest struct {
+	desc          string
+	counter, want []byte
+	overflow      bool
+}
+
 func TestCounterInc(t *testing.T) {
-	for _, test := range []struct {
-		desc          string
-		counter, want []byte
-		overflow      bool
-	}{
+	for _, test := range []counterTest{
 		{
 			desc:    "basic 1",
 			counter: testutil.Dehex("0000000000000000"),
@@ -54,33 +56,63 @@ func TestCounterInc(t *testing.T) {
 			overflow: true,
 		},
 	} {
+		// Test first getAndIncrement call. This should return the same value
+		// which was given.
 		c := counterFromValue(test.counter)
-		c.inc()
-		value, err := c.val()
+		value, err := c.getAndIncrement()
+		if err != nil {
+			t.Errorf("counter(%v).getAndIncrement() returned error: %v", test.counter, err)
+		}
+		if !bytes.Equal(value, test.counter) {
+			t.Errorf("counter(%v).getAndIncrement() = %v, want %v", test.counter, value, test.counter)
+		}
+
+		// Test second getAndIncrement call. This should verify that the first
+		// getAndIncrement call succeeded in incrementing the value.
+		value, err = c.getAndIncrement()
 		if test.overflow {
 			if err == nil {
-				t.Errorf("counter(%v).val() expected error, received none", test.counter)
+				t.Errorf("counter(%v).getAndIncrement() expected error, received none", test.counter)
 			}
 		} else {
 			if err != nil {
-				t.Errorf("counter(%v).val() returned error: %v", test.counter, err)
+				t.Errorf("counter(%v).getAndIncrement() returned error: %v", test.counter, err)
 			}
 			if !bytes.Equal(value, test.want) {
-				t.Errorf("counter(%v).val() = %v, want %v", test.counter, value, test.want)
+				t.Errorf("counter(%v).getAndIncrement() = %v, want %v", test.counter, value, test.want)
 			}
 			if c.hasOverflowed {
-				t.Errorf("counter(%v).val() unexpectedly set hasOverflowed flag", test.counter)
+				t.Errorf("counter(%v).getAndIncrement() unexpectedly set hasOverflowed flag", test.counter)
 			}
+		}
+	}
+}
+
+func TestCounterReset(t *testing.T) {
+	for _, test := range []counterTest{
+		{
+			desc:    "basic reset",
+			counter: testutil.Dehex("0100000000000000"),
+		},
+		{
+			desc:    "reset after overflow",
+			counter: testutil.Dehex("ffffffffffffffff"),
+		},
+	} {
+		c := counterFromValue(test.counter)
+		_, err := c.getAndIncrement()
+		if err != nil {
+			t.Errorf("counter(%v).getAndIncrement() returned error: %v", test.counter, err)
 		}
 
 		// Check that resetting the counter works as expected.
 		c.reset()
-		value, err = c.val()
+		value, err := c.getAndIncrement()
 		if err != nil {
 			t.Errorf("counter returned an error after resetting: %v", err)
 		}
 		if binary.LittleEndian.Uint64(value) != 0 {
-			t.Errorf("counter(%v).reset.val() = %v, expected 0", test.counter, err)
+			t.Errorf("counter(%v).reset.getAndIncrement() = %v, expected 0", test.counter, err)
 		}
 	}
 }
