ec60e9c15ca3f2c636cbe55aad78d3a4fa119811
Davis Gu <gud@google.com>
2020-06-08 22:09:10 +0000

    updated aesgcm format and variables to match CCP's

--- security/s2a/internal/crypter/aesgcm_test.go
+++ security/s2a/internal/crypter/aesgcm_test.go
@@ -19,10 +19,10 @@
 package crypter
 
 import (
-	"bytes"
 	"fmt"
-	"google.golang.org/grpc/security/s2a/internal/crypter/testutil"
 	"testing"
+
+	"google.golang.org/grpc/security/s2a/internal/crypter/testutil"
 )
 
 // getGCMCryptoPair outputs a sender/receiver pair on AES-GCM.
@@ -36,11 +36,6 @@ func getGCMCryptoPair(key []byte, t *testing.T) (s2aAeadCrypter, s2aAeadCrypter)
 		t.Fatalf("newAESGCM(ServerSide, key) = %v", err)
 	}
 	return sender, receiver
-}
-
-func isFailure(result string, err error, got, expected []byte) bool {
-	return (result == testutil.ValidResult && (err != nil || !bytes.Equal(got, expected))) ||
-		(result == testutil.InvalidResult && bytes.Equal(got, expected))
 }
 
 // wycheProofTestVectorFilter filters out unsupported wycheproof test vectors.
@@ -62,14 +57,14 @@ func testGCMEncryptionDecryption(sender s2aAeadCrypter, receiver s2aAeadCrypter,
 		dst = make([]byte, len(test.Plaintext)+sender.tagSize())
 	}
 	got, err := sender.encrypt(dst[:0], test.Plaintext, test.Nonce, test.Aad)
-	if isFailure(test.Result, err, got, ciphertext) {
+	if testutil.IsFailure(test.Result, err, got, ciphertext) {
 		t.Errorf("key=%v\nEncrypt(\n dst = %v\n plaintext = %v\n nonce = %v\n aad = %v\n) = (\n %v\n %v\n), want %v",
 			test.Key, dst[:0], test.Plaintext, test.Nonce, test.Aad, got, err, ciphertext)
 	}
 
 	// Decrypt.
 	got, err = receiver.decrypt(nil, ciphertext, test.Nonce, test.Aad)
-	if isFailure(test.Result, err, got, test.Plaintext) {
+	if testutil.IsFailure(test.Result, err, got, test.Plaintext) {
 		t.Errorf("key=%v\nDecrypt(\n dst = nil\n ciphertext = %v\n nonce = %v\n aad = %v\n) = (\n %v\n %v\n), want %v",
 			test.Key, ciphertext, test.Nonce, test.Aad, got, err, test.Plaintext)
 	}
