119c1201d4a2cf4a6aab18ed046ea42cc7363fd0
Ryan Kim <ryanfsdf@live.ca>
2020-06-11 10:48:57 -0600

    Add unit tests and comments

--- security/s2a/internal/crypter/halfconn.go
+++ security/s2a/internal/crypter/halfconn.go
@@ -8,13 +8,16 @@ import (
 	"sync"
 )
 
+// uint64Size is the size of a uint64 in bytes.
+const uint64Size = 8
+
 const (
 	tls13Key    = "tls13 key"
 	tls13Nonce  = "tls13 iv"
-	tls13Update = "tls13 upd"
+	tls13Update = "tls13 traffic upd"
 )
 
-type s2aHalfConnection struct {
+type S2AHalfConnection struct {
 	cs            ciphersuite
 	h             func() hash.Hash
 	aeadCrypter   s2aAeadCrypter
@@ -23,36 +26,35 @@ type s2aHalfConnection struct {
 	mutex         sync.Mutex
 	trafficSecret []byte
 	nonce         []byte
+	key           []byte
 }
 
-func newHalfConn(ciphersuite s2a_proto.Ciphersuite, trafficSecret []byte) (s2aHalfConnection, error) {
+// NewHalfConn creates a new instance of S2AHalfConnection.
+func NewHalfConn(ciphersuite s2a_proto.Ciphersuite, trafficSecret []byte) (S2AHalfConnection, error) {
 	cs := newCiphersuite(ciphersuite)
 	if cs.trafficSecretSize() != len(trafficSecret) {
-		return s2aHalfConnection{}, fmt.Errorf("supplied traffic secret must be %v bytes, given: %v", cs.trafficSecretSize(), trafficSecret)
+		return S2AHalfConnection{}, fmt.Errorf("supplied traffic secret must be %v bytes, given: %v", cs.trafficSecretSize(), trafficSecret)
 	}
 
-	hc := s2aHalfConnection{cs: cs, h: cs.hashFunction(), expander: &defaultHKDFExpander{}, seqCounter: newCounter()}
+	hc := S2AHalfConnection{cs: cs, h: cs.hashFunction(), expander: &defaultHKDFExpander{}, seqCounter: newCounter(), trafficSecret: trafficSecret}
 
-	key, err := hc.deriveSecret(trafficSecret, []byte(tls13Key), hc.cs.keySize())
-	if err != nil {
-		return s2aHalfConnection{}, fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", trafficSecret, tls13Key, hc.cs.keySize(), err)
+	var err error
+	if err = hc.updateWithNewTrafficSecret(hc.trafficSecret); err != nil {
+		return S2AHalfConnection{}, fmt.Errorf("hc.updateWithNewTrafficSecret(%v) failed with error: %v", hc.trafficSecret, err)
 	}
 
-	hc.nonce, err = hc.deriveSecret(trafficSecret, []byte(tls13Nonce), hc.cs.nonceSize())
+	hc.aeadCrypter, err = cs.aeadCrypter(hc.key)
 	if err != nil {
-		return s2aHalfConnection{}, fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", trafficSecret, tls13Nonce, hc.cs.nonceSize(), err)
-	}
-
-	hc.aeadCrypter, err = cs.aeadCrypter(key)
-	if err != nil {
-		return s2aHalfConnection{}, fmt.Errorf("cs.aeadCrypter(%v) failed with error: %v", key, err)
+		return S2AHalfConnection{}, fmt.Errorf("cs.aeadCrypter(%v) failed with error: %v", hc.key, err)
 	}
 	return hc, nil
 }
 
-// encrypt encrypts the plaintext and computes the tag of dst and plaintext.
-// dst and plaintext may fully overlap or not at all.
-func (hc *s2aHalfConnection) encrypt(dst, plaintext, aad []byte) ([]byte, error) {
+// Encrypt encrypts the plaintext and computes the tag of dst and plaintext.
+// dst and plaintext may fully overlap or not at all. Note that the sequence
+// number will still be incremented on failure, unless the sequence has
+// overflowed.
+func (hc *S2AHalfConnection) Encrypt(dst, plaintext, aad []byte) ([]byte, error) {
 	hc.mutex.Lock()
 	sequence, err := hc.getAndIncrementSequence()
 	if err != nil {
@@ -65,9 +67,10 @@ func (hc *s2aHalfConnection) encrypt(dst, plaintext, aad []byte) ([]byte, error)
 	return crypter.encrypt(dst, plaintext, nonce, aad)
 }
 
-// decrypt decrypts ciphertext and verifies the tag. dst and ciphertext may
-// fully overlap or not at all.
-func (hc *s2aHalfConnection) decrypt(dst, ciphertext, aad []byte) ([]byte, error) {
+// Decrypt decrypts ciphertext and verifies the tag. dst and ciphertext may
+// fully overlap or not at all. Note that the sequence number will still be
+// incremented on failure, unless the sequence has overflowed.
+func (hc *S2AHalfConnection) Decrypt(dst, ciphertext, aad []byte) ([]byte, error) {
 	hc.mutex.Lock()
 	sequence, err := hc.getAndIncrementSequence()
 	if err != nil {
@@ -80,9 +83,9 @@ func (hc *s2aHalfConnection) decrypt(dst, ciphertext, aad []byte) ([]byte, error
 	return crypter.decrypt(dst, ciphertext, nonce, aad)
 }
 
-// updateKey updates the traffic secret key, as specified in
+// UpdateKey updates the traffic secret key, as specified in
 // https://tools.ietf.org/html/rfc8446#section-7.2
-func (hc *s2aHalfConnection) updateKey() error {
+func (hc *S2AHalfConnection) UpdateKey() error {
 	hc.mutex.Lock()
 	defer hc.mutex.Unlock()
 
@@ -92,26 +95,37 @@ func (hc *s2aHalfConnection) updateKey() error {
 		return fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", hc.trafficSecret, tls13Update, hc.cs.trafficSecretSize(), err)
 	}
 
-	key, err := hc.deriveSecret(hc.trafficSecret, []byte(tls13Key), hc.cs.keySize())
-	if err != nil {
-		return fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", hc.trafficSecret, tls13Key, hc.cs.keySize(), err)
+	if err = hc.updateWithNewTrafficSecret(hc.trafficSecret); err != nil {
+		return fmt.Errorf("hc.updateWithNewTrafficSecret(%v) failed with error: %v", hc.trafficSecret, err)
 	}
 
-	hc.nonce, err = hc.deriveSecret(hc.trafficSecret, []byte(tls13Nonce), hc.cs.nonceSize())
+	err = hc.aeadCrypter.updateKey(hc.key)
 	if err != nil {
-		return fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", hc.trafficSecret, tls13Nonce, hc.cs.nonceSize(), err)
-	}
-
-	err = hc.aeadCrypter.updateKey(key)
-	if err != nil {
-		return fmt.Errorf("hc.aeadCrypter.updateKey(%v) failed with error: %v", key, err)
+		return fmt.Errorf("hc.aeadCrypter.updateKey(%v) failed with error: %v", hc.key, err)
 	}
 
 	hc.seqCounter.reset()
 	return nil
 }
 
-func (hc *s2aHalfConnection) getAndIncrementSequence() (uint64, error) {
+// updateWithNewTrafficSecret takes a new traffic secret and updates the key
+// and nonce.
+func (hc *S2AHalfConnection) updateWithNewTrafficSecret(newTrafficSecret []byte) error {
+	var err error
+	hc.key, err = hc.deriveSecret(newTrafficSecret, []byte(tls13Key), hc.cs.keySize())
+	if err != nil {
+		return fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", hc.trafficSecret, tls13Key, hc.cs.keySize(), err)
+	}
+
+	hc.nonce, err = hc.deriveSecret(newTrafficSecret, []byte(tls13Nonce), hc.cs.nonceSize())
+	if err != nil {
+		return fmt.Errorf("hc.deriveSecret(h, %v, %v, %v) failed with error: %v", hc.trafficSecret, tls13Nonce, hc.cs.nonceSize(), err)
+	}
+	return nil
+}
+
+// getAndIncrement returns the current sequence number and increments it.
+func (hc *S2AHalfConnection) getAndIncrementSequence() (uint64, error) {
 	sequence, err := hc.seqCounter.value()
 	if err != nil {
 		return 0, err
@@ -120,23 +134,28 @@ func (hc *s2aHalfConnection) getAndIncrementSequence() (uint64, error) {
 	return sequence, nil
 }
 
-func (hc *s2aHalfConnection) maskedNonce(sequence uint64) []byte {
+// maskedNonce creates a new S2A nonce using the sequence number.
+func (hc *S2AHalfConnection) maskedNonce(sequence uint64) []byte {
 	nonce := make([]byte, len(hc.nonce))
 	copy(nonce, hc.nonce)
-	// Note that the 8 represents the size of a uint64 in bytes.
-	for i := 0; i < 8; i++ {
-		nonce[nonceSize-8+i] ^= byte(sequence >> uint64(56-8*i))
+	for i := 0; i < uint64Size; i++ {
+		nonce[nonceSize-uint64Size+i] ^= byte(sequence >> uint64(56-uint64Size*i))
 	}
 	return nonce
 }
 
 // deriveSecret implements Derive-Secret specified in
 // https://tools.ietf.org/html/rfc8446#section-7.1.
-func (hc *s2aHalfConnection) deriveSecret(secret, label []byte, length int) ([]byte, error) {
+func (hc *S2AHalfConnection) deriveSecret(secret, label []byte, length int) ([]byte, error) {
 	var hkdfLabel cryptobyte.Builder
 	hkdfLabel.AddUint16(uint16(length))
 	hkdfLabel.AddUint8LengthPrefixed(func(b *cryptobyte.Builder) {
 		b.AddBytes(label)
+	})
+	// Append empty `Context` field, specified in the RFC. The Half Connection
+	// does not use the `Context` field.
+	hkdfLabel.AddUint8LengthPrefixed(func(b *cryptobyte.Builder) {
+		b.AddBytes([]byte(""))
 	})
 	hkdfLabelBytes, err := hkdfLabel.Bytes()
 	if err != nil {
--- security/s2a/internal/crypter/halfconn_test.go
+++ security/s2a/internal/crypter/halfconn_test.go
@@ -1,6 +1,7 @@
 package crypter
 
 import (
+	"bytes"
 	"fmt"
 	"google.golang.org/grpc/security/s2a/internal/crypter/testutil"
 	s2a_proto "google.golang.org/grpc/security/s2a/internal/proto"
@@ -8,61 +9,61 @@ import (
 )
 
 // getHalfConnPair returns a sender/receiver pair of S2A Half Connections.
-func getHalfConnPair(ciphersuite s2a_proto.Ciphersuite, trafficSecret []byte, t *testing.T) (s2aHalfConnection, s2aHalfConnection) {
-	sender, err := newHalfConn(ciphersuite, trafficSecret)
+func getHalfConnPair(ciphersuite s2a_proto.Ciphersuite, trafficSecret []byte, t *testing.T) (S2AHalfConnection, S2AHalfConnection) {
+	sender, err := NewHalfConn(ciphersuite, trafficSecret)
 	if err != nil {
-		t.Fatalf("sender side newHalfConn(%v, %v) failed, err = %v", ciphersuite, trafficSecret, err)
+		t.Fatalf("sender side NewHalfConn(%v, %v) failed, err = %v", ciphersuite, trafficSecret, err)
 	}
-	receiver, err := newHalfConn(ciphersuite, trafficSecret)
+	receiver, err := NewHalfConn(ciphersuite, trafficSecret)
 	if err != nil {
-		t.Fatalf("receiver side newHalfConn(%v, %v) failed, err = %v", ciphersuite, trafficSecret, err)
+		t.Fatalf("receiver side NewHalfConn(%v, %v) failed, err = %v", ciphersuite, trafficSecret, err)
 	}
 	return sender, receiver
 }
 
-func testHalfConnRoundtrip(sender s2aHalfConnection, receiver s2aHalfConnection, t *testing.T) {
+func testHalfConnRoundtrip(sender S2AHalfConnection, receiver S2AHalfConnection, t *testing.T) {
 	// Encrypt first message.
 	const plaintext = "This is plaintext."
 	buf := []byte(plaintext)
-	_, err := sender.encrypt(buf[:0], buf, nil)
+	_, err := sender.Encrypt(buf[:0], buf, nil)
 	if err != nil {
-		t.Fatalf("encrypt(%v, %v, nil) failed, err = %v", buf[:0], buf, err)
+		t.Fatalf("Encrypt(%v, %v, nil) failed, err = %v", buf[:0], buf, err)
 	}
 
 	// Encrypt second message.
 	const plaintext2 = "This is a second plaintext."
 	buf2 := []byte(plaintext2)
-	ciphertext2, err := sender.encrypt(buf2[:0], buf2, nil)
+	ciphertext2, err := sender.Encrypt(buf2[:0], buf2, nil)
 	if err != nil {
-		t.Fatalf("encrypt(%v, %v, nil) failed, err = %v", buf2[:0], buf2, err)
+		t.Fatalf("Encrypt(%v, %v, nil) failed, err = %v", buf2[:0], buf2, err)
 	}
 
 	// Decryption fails: cannot decrypt second message before first.
-	if _, err := receiver.decrypt(nil, ciphertext2, nil); err == nil {
-		t.Errorf("decrypt(nil, %v, nil) expected an error, received none", ciphertext2)
+	if _, err := receiver.Decrypt(nil, ciphertext2, nil); err == nil {
+		t.Errorf("Decrypt(nil, %v, nil) expected an error, received none", ciphertext2)
 	}
 
 	// Decrypt second message. This works now because the sequence number was
 	// incremented by the previous call to decrypt.
-	decryptedPlaintext2, err := receiver.decrypt(ciphertext2[:0], ciphertext2, nil)
+	decryptedPlaintext2, err := receiver.Decrypt(ciphertext2[:0], ciphertext2, nil)
 	if err != nil {
-		t.Fatalf("decrypt(%v, %v, nil) failed, err = %v", ciphertext2[:0], ciphertext2, err)
+		t.Fatalf("Decrypt(%v, %v, nil) failed, err = %v", ciphertext2[:0], ciphertext2, err)
 	}
 	if got, want := string(decryptedPlaintext2), plaintext2; got != want {
-		t.Fatalf("decrypt(%v, %v, nil) = %v, want %v", ciphertext2[:0], ciphertext2, got, want)
+		t.Fatalf("Decrypt(%v, %v, nil) = %v, want %v", ciphertext2[:0], ciphertext2, got, want)
 	}
 
 	// Decryption fails: same message decrypted again.
-	if _, err := receiver.decrypt(nil, ciphertext2, nil); err == nil {
-		t.Errorf("decrypt(nil, %v, nil) expected an error, received none", ciphertext2)
+	if _, err := receiver.Decrypt(nil, ciphertext2, nil); err == nil {
+		t.Errorf("Decrypt(nil, %v, nil) expected an error, received none", ciphertext2)
 	}
 }
 
 func TestNewHalfConn(t *testing.T) {
 	for _, tc := range []struct {
-		ciphersuite   s2a_proto.Ciphersuite
-		trafficSecret []byte
-		shouldFail    bool
+		ciphersuite               s2a_proto.Ciphersuite
+		trafficSecret, key, nonce []byte
+		shouldFail                bool
 	}{
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_AES_128_GCM_SHA256,
@@ -72,6 +73,8 @@ func TestNewHalfConn(t *testing.T) {
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_AES_128_GCM_SHA256,
 			trafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			key:           testutil.Dehex("c3ae7509cfced2b803a6186956cda79f"),
+			nonce:         testutil.Dehex("b5803d82ad8854d2e598187f"),
 		},
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_AES_256_GCM_SHA384,
@@ -81,6 +84,8 @@ func TestNewHalfConn(t *testing.T) {
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_AES_256_GCM_SHA384,
 			trafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			key:           testutil.Dehex("dac731ae4866677ed2f65c490e18817be5cbbbd03f597ad59041c117b731109a"),
+			nonce:         testutil.Dehex("4db152d27d180b1ee48fa89d"),
 		},
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256,
@@ -90,24 +95,102 @@ func TestNewHalfConn(t *testing.T) {
 		{
 			ciphersuite:   s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256,
 			trafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			key:           testutil.Dehex("130e2000508ace00ef265e172d09892e467256cb90dad9de99543cf548be6a8b"),
+			nonce:         testutil.Dehex("b5803d82ad8854d2e598187f"),
 		},
 	} {
 		t.Run(fmt.Sprintf("%v/shouldFail=%v", tc.ciphersuite.String(), tc.shouldFail), func(t *testing.T) {
-			// TODO(rnkim): Remove below.
-			if tc.ciphersuite == s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256 {
-				return
-			}
-
-			_, err := newHalfConn(tc.ciphersuite, tc.trafficSecret)
+			hc, err := NewHalfConn(tc.ciphersuite, tc.trafficSecret)
 			if got, want := err == nil, !tc.shouldFail; got != want {
-				t.Errorf("newHalfConn(%v, %v)=(err=nil)=%v, want %v", tc.ciphersuite, tc.trafficSecret, got, want)
+				t.Errorf("NewHalfConn(%v, %v)=(err=nil)=%v, want %v", tc.ciphersuite, tc.trafficSecret, got, want)
 			}
-
+			if err == nil {
+				// Check that the traffic secret wasn't changed.
+				if got, want := hc.trafficSecret, tc.trafficSecret; !bytes.Equal(got, want) {
+					t.Errorf("NewHalfConn(%v, %v).trafficSecret=%v, want %v", tc.ciphersuite, tc.trafficSecret, got, want)
+				}
+				if got, want := hc.key, tc.key; !bytes.Equal(got, want) {
+					t.Errorf("NewHalfConn(%v, %v).key=%v, want %v", tc.ciphersuite, tc.trafficSecret, got, want)
+				}
+				if got, want := hc.nonce, tc.nonce; !bytes.Equal(got, want) {
+					t.Errorf("NewHalfConn(%v, %v).nonce=%v, want %v", tc.ciphersuite, tc.trafficSecret, got, want)
+				}
+			}
 		})
 	}
 }
 
-func TestS2AHalfConnectionEncryptDecrypt(t *testing.T) {
+func TestS2AHalfConnectionEncrypt(t *testing.T) {
+	for _, tc := range []struct {
+		ciphersuite                                                  s2a_proto.Ciphersuite
+		trafficSecret                                                []byte
+		expectedCiphertext, expectedCiphertext2, expectedCiphertext3 []byte
+	}{
+		{
+			ciphersuite:         s2a_proto.Ciphersuite_AES_128_GCM_SHA256,
+			trafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedCiphertext:  testutil.Dehex("f2e4e411ac674e01385e7ae9db54c97a9ae3d1842e51"),
+			expectedCiphertext2: testutil.Dehex("d7853afd6d7ceaababded0348f9a9c3a5b52544f0033d7c7ad"),
+			expectedCiphertext3: testutil.Dehex("af778b0e98365c4ee2174f17aab4aa0aba58eab1"),
+		},
+		{
+			ciphersuite:         s2a_proto.Ciphersuite_AES_256_GCM_SHA384,
+			trafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedCiphertext:  testutil.Dehex("24efee5af1a665e6bb188e79544a7e974c707d690c5f"),
+			expectedCiphertext2: testutil.Dehex("832a5fd271b6442e743d6e30dcd66441846e719143b2acb9f4"),
+			expectedCiphertext3: testutil.Dehex("96cb84cc897caf296f9663fbc376243c7b53239c"),
+		},
+		{
+			ciphersuite:         s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256,
+			trafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedCiphertext:  testutil.Dehex("c947ffa470308b19d9a086b05ccb75d8c94abb704aae"),
+			expectedCiphertext2: testutil.Dehex("0cedeb922170c110c1f11bc03ffffbb2f5a9393d51b8d52f14"),
+			expectedCiphertext3: testutil.Dehex("924a53438593d1d76e54799b9ee8f35f2cee20dc"),
+		},
+	} {
+		t.Run(tc.ciphersuite.String(), func(t *testing.T) {
+			hc, err := NewHalfConn(tc.ciphersuite, tc.trafficSecret)
+			if err != nil {
+				t.Fatalf("NewHalfConn(%v, %v) failed, err = %v", tc.ciphersuite, tc.trafficSecret, err)
+			}
+
+			// Test encrypt with sequence 0.
+			const plaintext = "123456"
+			buf := []byte(plaintext)
+			ciphertext, err := hc.Encrypt(buf[:0], buf, nil)
+			if err != nil {
+				t.Fatalf("Encrypt(%v, %v, nil) failed, err = %v", buf[:0], buf, err)
+			}
+			if got, want := ciphertext, tc.expectedCiphertext; !bytes.Equal(got, want) {
+				t.Fatalf("Encrypt(%v, %v, nil) = %v, want %v", buf[:0], buf, got, want)
+			}
+
+			// Test encrypt with sequence 1.
+			const plaintext2 = "789123456"
+			buf2 := []byte(plaintext2)
+			ciphertext2, err := hc.Encrypt(buf2[:0], buf2, nil)
+			if err != nil {
+				t.Fatalf("Encrypt(%v, %v, nil) failed, err = %v", buf2[:0], buf2, err)
+			}
+			if got, want := ciphertext2, tc.expectedCiphertext2; !bytes.Equal(got, want) {
+				t.Fatalf("Encrypt(%v, %v, nil) = %v, want %v", buf2[:0], buf2, got, want)
+			}
+
+			// Test encrypt with sequence 2.
+			const plaintext3 = "7891"
+			buf3 := []byte(plaintext3)
+			ciphertext3, err := hc.Encrypt(buf3[:0], buf3, nil)
+			if err != nil {
+				t.Fatalf("Encrypt(%v, %v, nil) failed, err = %v", buf3[:0], buf3, err)
+			}
+			if got, want := ciphertext3, tc.expectedCiphertext3; !bytes.Equal(got, want) {
+				t.Fatalf("Encrypt(%v, %v, nil) = %v, want %v", buf3[:0], buf3, got, want)
+			}
+		})
+	}
+}
+
+func TestS2AHalfConnectionRoundtrip(t *testing.T) {
 	for _, tc := range []struct {
 		ciphersuite   s2a_proto.Ciphersuite
 		trafficSecret []byte
@@ -126,17 +209,56 @@ func TestS2AHalfConnectionEncryptDecrypt(t *testing.T) {
 		},
 	} {
 		t.Run(tc.ciphersuite.String(), func(t *testing.T) {
-			// TODO(rnkim): Remove below.
-			if tc.ciphersuite == s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256 {
-				return
-			}
 			sender, receiver := getHalfConnPair(tc.ciphersuite, tc.trafficSecret, t)
 			testHalfConnRoundtrip(sender, receiver, t)
 		})
 	}
-
 }
 
 func TestS2AHalfConnectionUpdateKey(t *testing.T) {
-
+	for _, tc := range []struct {
+		ciphersuite                                      s2a_proto.Ciphersuite
+		trafficSecret, expectedTrafficSecret, key, nonce []byte
+	}{
+		{
+			ciphersuite:           s2a_proto.Ciphersuite_AES_128_GCM_SHA256,
+			trafficSecret:         testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedTrafficSecret: testutil.Dehex("f38b9455ea5871235a69fc37610c6ca1215779e66b45a047d7390111e00081c4"),
+			key:                   testutil.Dehex("07dfdfca2fc3f015b6e51e579679b503"),
+			nonce:                 testutil.Dehex("79fdebc61b5fb9d9a34d9406"),
+		},
+		{
+			ciphersuite:           s2a_proto.Ciphersuite_AES_256_GCM_SHA384,
+			trafficSecret:         testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedTrafficSecret: testutil.Dehex("016c835db664beb5526a9bb3d9a4fba63e67255dcfa460a114d9f1ef9a9a1f685a518739f557d0e66fdb89bdafa26257"),
+			key:                   testutil.Dehex("4ee0f141c9a497a1db6f1ee0995248e804406fe39f35bcdff9f386048108bef1"),
+			nonce:                 testutil.Dehex("90f241fbc9f9f55100168d8b"),
+		},
+		{
+			ciphersuite:           s2a_proto.Ciphersuite_CHACHA20_POLY1305_SHA256,
+			trafficSecret:         testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+			expectedTrafficSecret: testutil.Dehex("f38b9455ea5871235a69fc37610c6ca1215779e66b45a047d7390111e00081c4"),
+			key:                   testutil.Dehex("18b61f93ee2d927d2f478f2220409738affb0092602d0812c96b965323e30878"),
+			nonce:                 testutil.Dehex("79fdebc61b5fb9d9a34d9406"),
+		},
+	} {
+		t.Run(tc.ciphersuite.String(), func(t *testing.T) {
+			hc, err := NewHalfConn(tc.ciphersuite, tc.trafficSecret)
+			if err != nil {
+				t.Fatalf("NewHalfConn(%v, %v) failed, err = %v", tc.ciphersuite, tc.trafficSecret, err)
+			}
+			if err := hc.UpdateKey(); err != nil {
+				t.Fatalf("hc.updateKey() failed, err = %v", err)
+			}
+			if got, want := hc.trafficSecret, tc.expectedTrafficSecret; !bytes.Equal(got, want) {
+				t.Errorf("updated traffic secret = %v, want %v", got, want)
+			}
+			if got, want := hc.key, tc.key; !bytes.Equal(got, want) {
+				t.Errorf("updated key = %v, want %v", got, want)
+			}
+			if got, want := hc.nonce, tc.nonce; !bytes.Equal(got, want) {
+				t.Errorf("updated nonce = %v, want %v", got, want)
+			}
+		})
+	}
 }
