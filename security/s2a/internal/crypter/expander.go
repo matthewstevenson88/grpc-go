2f72451ba696f82ee292b1e1f228ddfa9fee749d
Ryan Kim <ryanfsdf@live.ca>
2020-06-19 17:17:01 -0600

    Update HKDF expander API

--- security/s2a/internal/crypter/expander.go
+++ security/s2a/internal/crypter/expander.go
@@ -10,23 +10,26 @@ import (
 // https://tools.ietf.org/html/rfc5869 for details. It's use in TLS 1.3 is
 // specified in https://tools.ietf.org/html/rfc8446#section-7.2
 type hkdfExpander interface {
-	// expand takes a hashing function, a secret, a label, and the output length
-	// in bytes, and returns the resulting expanded key.
-	expand(h func() hash.Hash, secret, label []byte, length int) ([]byte, error)
+	// expand takes a secret, a label, and the output length in bytes, and
+	// returns the resulting expanded key.
+	expand(secret, label []byte, length int) ([]byte, error)
 }
 
 // defaultHKDFExpander is the default HKDF expander which uses Go's crypto/hkdf
 // for HKDF expansion.
-type defaultHKDFExpander struct{}
-
-// newDefaultHKDFExpander creates an instance of the default HKDF expander.
-func newDefaultHKDFExpander() hkdfExpander {
-	return &defaultHKDFExpander{}
+type defaultHKDFExpander struct {
+	h func() hash.Hash
 }
 
-func (*defaultHKDFExpander) expand(h func() hash.Hash, secret, label []byte, length int) ([]byte, error) {
+// newDefaultHKDFExpander creates an instance of the default HKDF expander
+// using the given hash function.
+func newDefaultHKDFExpander(h func() hash.Hash) hkdfExpander {
+	return &defaultHKDFExpander{h: h}
+}
+
+func (d *defaultHKDFExpander) expand(secret, label []byte, length int) ([]byte, error) {
 	outBuf := make([]byte, length)
-	n, err := hkdf.Expand(h, secret, label).Read(outBuf)
+	n, err := hkdf.Expand(d.h, secret, label).Read(outBuf)
 	if err != nil {
 		return nil, fmt.Errorf("hkdf.Expand.Read failed with error: %v", err)
 	}
