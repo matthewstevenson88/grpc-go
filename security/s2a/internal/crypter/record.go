545a7365777c8dd293e04b8ff8da718ad16064ea
Ryan Kim <ryanfsdf@live.ca>
2020-06-22 14:12:12 -0600

    Address review comments, @cesar

--- security/s2a/internal/crypter/record.go
+++ security/s2a/internal/crypter/record.go
@@ -20,98 +20,95 @@ const (
 	// tlsRecordTypeSize is the size in bytes of the TLS 1.3 record type.
 	tlsRecordTypeSize = 1
 	// TODO(gud): Revisit what initial size to use when implementating Write.
-	// s2aOutgoingRecordsBufInitialSize is the initial write buffer size in
-	// bytes.
-	s2aOutgoingRecordsBufInitialSize = 32 * 1024
+	// outBufSize is the initial write buffer size in bytes.
+	outBufSize = 32 * 1024
 )
 
 // conn represents a secured TLS connection. It implements the net.Conn
 // interface.
 type conn struct {
 	net.Conn
-	// inConnection is the half connection responsible for decrypting incoming
-	// bytes.
-	inConnection *S2AHalfConnection
-	// outConnection is the half connection responsible for encrypting outgoing
-	// bytes.
-	outConnection *S2AHalfConnection
+	// inConn is the half connection responsible for decrypting incoming bytes.
+	inConn *S2AHalfConnection
+	// outConn is the half connection responsible for encrypting outgoing bytes.
+	outConn *S2AHalfConnection
 	// pendingApplicationData holds data that has been read from the connection
 	// and decrypted, but has not yet been returned by Read.
 	pendingApplicationData []byte
-	// unusedBytes holds data read from the network that has not yet been
+	// unusedBuf holds data read from the network that has not yet been
 	// decrypted. This data might not consist of a complete record. It may
 	// consist of several records, the last of which could be incomplete.
-	unusedBytes []byte
-	// outgoingRecordsBuf is a buffer used to contain outgoing TLS records
-	// before they are written to the network.
-	outgoingRecordsBuf []byte
-	// nextRecord stores the next record info in the unusedBytes buffer.
+	unusedBuf []byte
+	// outRecordsBuf is a buffer used to contain outgoing TLS records before
+	// they are written to the network.
+	outRecordsBuf []byte
+	// nextRecord stores the next record info in the unusedBuf buffer.
 	nextRecord []byte
 	// overheadSize is the overhead size in bytes of each TLS 1.3 record, which
 	// is computed as overheadSize = header size + record type byte + tag size.
 	// Note that there is no padding by zeros in the overhead calculation.
 	overheadSize int
-	// handshakerServiceAddr stores the address of the S2A handshaker service.
-	handshakerServiceAddr string
+	// hsAddr stores the address of the S2A handshaker service.
+	hsAddr string
 }
 
 // ConnOptions holds the options used for creating a new conn object.
 type ConnOptions struct {
-	netConn                                        net.Conn
-	ciphersuite                                    s2apb.Ciphersuite
-	tlsVersion                                     s2apb.TLSVersion
-	inTrafficSecret, outTrafficSecret, unusedBytes []byte
+	netConn                                      net.Conn
+	ciphersuite                                  s2apb.Ciphersuite
+	tlsVersion                                   s2apb.TLSVersion
+	inTrafficSecret, outTrafficSecret, unusedBuf []byte
 	// TODO(rnkim): Add initial sequence number to half conneciton.
 	inSequence, outSequence uint64
-	handshakerServiceAddr   string
+	hsAddr                  string
 }
 
 func NewConn(o *ConnOptions) (net.Conn, error) {
 	if o == nil {
-		return nil, errors.New("conn options must be not nil")
+		return nil, errors.New("conn options must not be nil")
 	}
 	if o.tlsVersion != s2apb.TLSVersion_TLS1_3 {
 		return nil, errors.New("TLS version must be TLS 1.3")
 	}
 
-	inConnection, err := NewHalfConn(o.ciphersuite, o.inTrafficSecret)
+	inConn, err := NewHalfConn(o.ciphersuite, o.inTrafficSecret)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create inbound half connection: %v", err)
 	}
-	outConnection, err := NewHalfConn(o.ciphersuite, o.outTrafficSecret)
+	outConn, err := NewHalfConn(o.ciphersuite, o.outTrafficSecret)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create outbound half connection: %v", err)
 	}
 
 	// TODO(rnkim): Add TagSize() to Half Connection.
-	// Note: The tag size for the in/out connections should be the same.
-	overheadSize := tlsRecordHeaderSize + tlsRecordTypeSize + inConnection.aeadCrypter.tagSize()
-	var unusedBytesBuf []byte
-	// TODO(gud): Potentially optimize unusedBytesBuf with pre-allocation.
-	if o.unusedBytes != nil {
-		unusedBytesBuf = make([]byte, len(o.unusedBytes))
-		copy(unusedBytesBuf, o.unusedBytes)
+	// The tag size for the in/out connections should be the same.
+	overheadSize := tlsRecordHeaderSize + tlsRecordTypeSize + inConn.aeadCrypter.tagSize()
+	var unusedBuf []byte
+	// TODO(gud): Potentially optimize unusedBuf with pre-allocation.
+	if o.unusedBuf != nil {
+		unusedBuf = make([]byte, len(o.unusedBuf))
+		copy(unusedBuf, o.unusedBuf)
 	}
 
 	s2aConn := &conn{
-		Conn:                  o.netConn,
-		inConnection:          inConnection,
-		outConnection:         outConnection,
-		unusedBytes:           unusedBytesBuf,
-		outgoingRecordsBuf:    make([]byte, s2aOutgoingRecordsBufInitialSize),
-		nextRecord:            unusedBytesBuf,
-		overheadSize:          overheadSize,
-		handshakerServiceAddr: o.handshakerServiceAddr,
+		Conn:          o.netConn,
+		inConn:        inConn,
+		outConn:       outConn,
+		unusedBuf:     unusedBuf,
+		outRecordsBuf: make([]byte, outBufSize),
+		nextRecord:    unusedBuf,
+		overheadSize:  overheadSize,
+		hsAddr:        o.hsAddr,
 	}
 	return s2aConn, nil
 }
 
 func (p *conn) Read(b []byte) (n int, err error) {
 	// TODO: Implement this.
-	panic("Read unimplemented")
+	return 0, errors.New("read unimplemented")
 }
 
 func (p *conn) Write(b []byte) (n int, err error) {
 	// TODO: Implement this.
-	panic("Write unimplemented")
+	return 0, errors.New("write unimplemented")
 }
--- security/s2a/internal/crypter/record_test.go
+++ security/s2a/internal/crypter/record_test.go
@@ -35,94 +35,98 @@ func TestNewS2ARecordConn(t *testing.T) {
 		{
 			desc: "invalid input traffic secret size",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_AES_256_GCM_SHA384,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_AES_256_GCM_SHA384,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
 			outErr: true,
 		},
 		{
 			desc: "invalid output traffic secret size",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_AES_256_GCM_SHA384,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_AES_256_GCM_SHA384,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
 			outErr: true,
 		},
 		{
 			desc: "invalid tls version",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_AES_128_GCM_SHA256,
-				tlsVersion:            s2apb.TLSVersion_TLS1_2,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_AES_128_GCM_SHA256,
+				tlsVersion:       s2apb.TLSVersion_TLS1_2,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
 			outErr: true,
 		},
 		{
 			desc: "basic with AES-128-GCM-SHA256",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_AES_128_GCM_SHA256,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_AES_128_GCM_SHA256,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
-			// outOverheadSize = header size + record type byte + tag size.
+			// outOverheadSize = header size (5) + record type byte (1) +
+			// tag size (16).
 			outOverheadSize:          22,
 			outHandshakerServiceAddr: "test handshaker address",
 		},
 		{
 			desc: "basic with AES-256-GCM-SHA384",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_AES_256_GCM_SHA384,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_AES_256_GCM_SHA384,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
-			// outOverheadSize = header size + record type byte + tag size.
+			// outOverheadSize = header size (5) + record type byte (1) +
+			// tag size (16).
 			outOverheadSize:          22,
 			outHandshakerServiceAddr: "test handshaker address",
 		},
 		{
 			desc: "basic with CHACHA20-POLY1305-SHA256",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_CHACHA20_POLY1305_SHA256,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_CHACHA20_POLY1305_SHA256,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				hsAddr:           "test handshaker address",
 			},
-			// outOverheadSize = header size + record type byte + tag size.
+			// outOverheadSize = header size (5) + record type byte (1) +
+			// tag size (16).
 			outOverheadSize:          22,
 			outHandshakerServiceAddr: "test handshaker address",
 		},
 		{
 			desc: "basic with unusedBytes",
 			options: &ConnOptions{
-				netConn:               &fakeConn{},
-				ciphersuite:           s2apb.Ciphersuite_CHACHA20_POLY1305_SHA256,
-				tlsVersion:            s2apb.TLSVersion_TLS1_3,
-				inTrafficSecret:       testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				outTrafficSecret:      testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
-				unusedBytes:           testutil.Dehex("ffffffff"),
-				handshakerServiceAddr: "test handshaker address",
+				netConn:          &fakeConn{},
+				ciphersuite:      s2apb.Ciphersuite_CHACHA20_POLY1305_SHA256,
+				tlsVersion:       s2apb.TLSVersion_TLS1_3,
+				inTrafficSecret:  testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				outTrafficSecret: testutil.Dehex("6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b6b"),
+				unusedBuf:        testutil.Dehex("ffffffff"),
+				hsAddr:           "test handshaker address",
 			},
 			outUnusedBytesBuf: testutil.Dehex("ffffffff"),
-			// outOverheadSize = header size + record type byte + tag size.
+			// outOverheadSize = header size (5) + record type byte (1) +
+			// tag size (16).
 			outOverheadSize:          22,
 			outHandshakerServiceAddr: "test handshaker address",
 		},
@@ -132,18 +136,33 @@ func TestNewS2ARecordConn(t *testing.T) {
 			if got, want := err == nil, !tc.outErr; got != want {
 				t.Errorf("NewConn(%v) = (err=nil) = %v, want %v", *tc.options, got, want)
 			}
-			if err == nil {
-				conn := netConn.(*conn)
-				if got, want := conn.unusedBytes, tc.outUnusedBytesBuf; !bytes.Equal(got, want) {
-					t.Errorf("conn.unusedBytes = %v, want %v", got, want)
-				}
-				if got, want := conn.overheadSize, tc.outOverheadSize; got != want {
-					t.Errorf("conn.overheadSize = %v, want %v", got, want)
-				}
-				if got, want := conn.handshakerServiceAddr, tc.outHandshakerServiceAddr; got != want {
-					t.Errorf("conn.handshakerServiceAddr = %v, want %v", got, want)
-				}
+			if err != nil {
+				return
+			}
+			conn := netConn.(*conn)
+			if got, want := conn.unusedBuf, tc.outUnusedBytesBuf; !bytes.Equal(got, want) {
+				t.Errorf("conn.unusedBytes = %v, want %v", got, want)
+			}
+			if got, want := conn.overheadSize, tc.outOverheadSize; got != want {
+				t.Errorf("conn.overheadSize = %v, want %v", got, want)
+			}
+			if got, want := conn.hsAddr, tc.outHandshakerServiceAddr; got != want {
+				t.Errorf("conn.hsAddr = %v, want %v", got, want)
 			}
 		})
+	}
+}
+
+func TestConnRead(t *testing.T) {
+	conn := &conn{}
+	if _, err := conn.Read(nil); err == nil {
+		t.Errorf("read is unimplemented")
+	}
+}
+
+func TestConnWrite(t *testing.T) {
+	conn := &conn{}
+	if _, err := conn.Write(nil); err == nil {
+		t.Errorf("write is unimplemented")
 	}
 }
