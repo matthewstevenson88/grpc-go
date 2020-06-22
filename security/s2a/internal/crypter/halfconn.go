274a87b8e5bba218696d714bb91b709a50ab98fc
Ryan Kim <ryanfsdf@live.ca>
2020-06-22 13:20:50 -0600

    Use constructor rather than manually construct

--- security/s2a/internal/crypter/halfconn.go
+++ security/s2a/internal/crypter/halfconn.go
@@ -38,7 +38,7 @@ func NewHalfConn(ciphersuite s2apb.Ciphersuite, trafficSecret []byte) (*S2AHalfC
 		return nil, fmt.Errorf("supplied traffic secret must be %v bytes, given: %v bytes", cs.trafficSecretSize(), len(trafficSecret))
 	}
 
-	hc := &S2AHalfConnection{cs: cs, expander: &defaultHKDFExpander{cs.hashFunction()}, sequence: newCounter(0), trafficSecret: trafficSecret}
+	hc := &S2AHalfConnection{cs: cs, expander: newDefaultHKDFExpander(cs.hashFunction()), sequence: newCounter(0), trafficSecret: trafficSecret}
 	if err = hc.updateCrypterAndNonce(hc.trafficSecret); err != nil {
 		return nil, fmt.Errorf("failed to create half connection using traffic secret: %v", err)
 	}
