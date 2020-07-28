a0c78b589cae4e13c6a4c5f59ccc83353f3954c7
Davis Gu <gud@google.com>
2020-07-28 19:06:43 +0000

    created e2e test server file

---
+++ security/s2a/examples/e2e_server/main.go
@@ -0,0 +1,69 @@
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/security/s2a"
+	pb "google.golang.org/grpc/security/s2a/examples/helloworld"
+)
+
+var (
+	port     = flag.String("port", "50050", "port number to use for connection")
+	s2a_port = flag.String("s2a_port", "50051", "port number to use for s2a connection")
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	// Set up server options.
+	serverOpts := &s2a.ServerOptions{
+		LocalIdentities:          []s2a.Identity{s2a.NewSpiffeID(*port)},
+		HandshakerServiceAddress: *s2a_port,
+	}
+	creds, err := s2a.NewServerCreds(serverOpts)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", *port))
+	if err != nil {
+		log.Fatalf("failed to listen on port %s: %v", *port, err)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
